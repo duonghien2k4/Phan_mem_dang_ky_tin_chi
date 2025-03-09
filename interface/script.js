0c93df468c43efe2293447c1970fa00da537dbe5
ngtiensdat <datga27@gmail.com>
2025-03-09 21:38:30 +0700

    trnag_chu

--- interface/script.js
+++ interface/script.js
@@ -12,4 +12,13 @@ function logout() {
 function contactSupport() {
     alert("Liên hệ hỗ trợ qua email: support@example.com");
 }
-
+document.addEventListener("DOMContentLoaded", function () {
+    showSection('home-info'); // Hiển thị Trang chủ khi tải trang
+});
+/* function showSection(sectionId) {
+    const sections = ["home-info", "student-info", "course-list", "teacher-list", "schedule", "registration"];
+    sections.forEach(id => {
+        document.getElementById(id).style.display = (id === sectionId) ? "block" : "none";
+    });
+}
+*/
