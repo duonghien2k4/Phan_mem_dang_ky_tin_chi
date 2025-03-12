7afc56538e86d21e5dd45d3ca24c895e59c81ff2
dtthien <22014071@st.phenikaa-uni.edu.vn>
2025-03-12 21:33:41 +0700

    ud

--- interface/script.js
+++ interface/script.js
@@ -1,12 +1,12 @@
 // Các thẻ ẩn hiển
-       function showSection(sectionId) {
-            document.querySelectorAll('.content > div').forEach(div => div.style.display = 'none');
-            document.getElementById(sectionId).style.display = 'block';
-        }
+function showSection(sectionId) {
+    document.querySelectorAll('.content > div').forEach(div => div.style.display = 'none');
+    document.getElementById(sectionId).style.display = 'block';
+}
 // log out
 function logout() {
     alert("Bạn đã đăng xuất thành công!");
-    window.location.href = "../sign_in/sign_in.html"; 
+    window.location.href = "../sign_in/sign_in.html";
 }
 
 function contactSupport() {
